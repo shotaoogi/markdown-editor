f0a5004187327aa1fb8b1fccf39297572698fa10
shotaoogi <shota.oogi@gmail.com>
2021-03-09 14:16:05 +0900

    ページ数を返す関数を追加

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -6,6 +6,14 @@ export interface MemoRecord {
   text: string
 }
 
+const NUM_PER_PAGE: number = 10
+
+export const getMemoPageCount = async (): Promise<number> => {
+  const totalCount = await memos.count()
+  const pageCount = Math.ceil(totalCount / NUM_PER_PAGE)
+  return pageCount > 0 ? pageCount : 1
+}
+
 const database = new Dexie('markdown-editor')
 database.version(1).stores({ memos: '&datetime' })
 const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
